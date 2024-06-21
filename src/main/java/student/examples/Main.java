d74414ac9a580872fec0c179dc88b783ae521237
Dencik <agafonov.denis11@gmail.com>
2024-06-21 14:39:48 +0300

    initial commit

--- src/main/java/student/examples/Main.java
+++ src/main/java/student/examples/Main.java
@@ -2,10 +2,6 @@ package student.examples;
 
 public class Main {
     public static void main(String[] args) {
-        Product product = new Product.ProductBuilder("Samsung", 1000.00)
-                .withWeight(124.6, "gramms")
-                .withDimension(2.5, 0.1, 4.5, "inch")
-                .build();
-        System.out.println(product);
+
     }
 }
